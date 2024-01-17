876eba03ab96c6b646d5f0206c22e8aca114d598
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-17 16:07:25 +0500

    added layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/ui/shared/navbar";
+import Sidebar from "@/components/ui/shared/sidebar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Academy',
-  description: 'Sukhrob Academy',
-}
+  title: "Academy",
+  description: "Sukhrob Academy",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Sidebar />
+      </body>
     </html>
-  )
+  );
 }
