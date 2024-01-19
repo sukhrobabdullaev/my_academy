c20b548885cc661048869bfb6bc6f38b92d106d3
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-19 20:43:56 +0500

    added sign-up and sig-in

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -11,12 +11,12 @@ export default function Login() {
     <div className="min-h-screen fixed top-0 right-0 w-full h-full flex items-center justify-center z-50">
       <div className="absolute bg-black/40 inset-0">
         {/* <Button className="m-4 bg-transparent"> */}
-        <ChevronLeftIcon
-          className="m-4 text-[#3bc43f] cursor-pointer w-6 h-6 transition-transform transform hover:translate-x-[-4px]"
-          onClick={() => router.back()}
-        />
         {/* </Button> */}
       </div>
+        <ChevronLeftIcon
+          className="flex items-center justify-center text-[#3bc43f] cursor-pointer w-6 h-6 font-bold transition-transform transform hover:translate-x-[-4px]"
+          onClick={() => router.back()}
+        />
       <SignIn />
     </div>
   );
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { SignUp } from "@clerk/nextjs";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
+
+export default function SignUpDemo() {
+  const router = useRouter();
+  return (
+    <div className="min-h-screen fixed top-0 right-0 w-full h-full flex items-center justify-center z-50">
+      <div className="absolute bg-black/40 inset-0"></div>
+      <ChevronLeftIcon
+        className="flex items-center justify-center text-[#3bc43f] cursor-pointer w-6 h-6 font-bold transition-transform transform hover:translate-x-[-4px]"
+        onClick={() => router.back()}
+      />
+      <SignUp />
+    </div>
+  );
+}
