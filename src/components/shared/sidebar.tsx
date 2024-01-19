2b1d7c6fcdb48be33b4de384d287fe0893a0e231
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-19 11:55:10 +0500

    added pathname by client component

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ArrowRightEndOnRectangleIcon,
   CodeBracketSquareIcon,
@@ -9,10 +11,13 @@ import {
 } from "@heroicons/react/16/solid";
 import { Ubuntu } from "next/font/google";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["300", "500"] });
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="bg-zinc-50 dark:bg-zinc-900 p-4 fixed top-14 w-64 left-0 h-screen border-r">
       <ul
@@ -20,7 +25,9 @@ const Sidebar = () => {
       >
         <Link
           href={"/"}
-          className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
+          className={`${
+            pathname === "/" ? "bg-[#3bc43f]" : ""
+          } flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800`}
         >
           <HomeIcon className="w-6 h-6" />
           Bosh Sahifa
@@ -30,28 +37,28 @@ const Sidebar = () => {
           className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
         >
           {/* <CodeBracketSquareIcon className="w-6 h-6"/> */}
-          <DocumentTextIcon className="w-6 h-6"/>
+          <DocumentTextIcon className="w-6 h-6" />
           Maqolalar
         </Link>
         <Link
           href={"/source_code"}
           className="flex text-lg items-center gap-1 border p-3 w-full mx-auto border-[#3bc43f] rounded-md hover:border-[#288829]"
         >
-          <CodeBracketSquareIcon className="w-6 h-6"/>
+          <CodeBracketSquareIcon className="w-6 h-6" />
           Kod Manbalari
         </Link>
         <Link
           href={"/courses"}
           className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
         >
-          <ComputerDesktopIcon className="w-6 h-6"/>
+          <ComputerDesktopIcon className="w-6 h-6" />
           Barch kurslar
         </Link>
         <Link
           href={"/projects"}
           className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
         >
-          <FolderIcon className="w-6 h-6"/>
+          <FolderIcon className="w-6 h-6" />
           Loyihalar
         </Link>
       </ul>
