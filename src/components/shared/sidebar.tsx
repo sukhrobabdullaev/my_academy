7e534709fda2245ce0330269692bd90e5791c9b6
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-19 11:41:36 +0500

    added sidebar

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -1,11 +1,66 @@
+import {
+  ArrowRightEndOnRectangleIcon,
+  CodeBracketSquareIcon,
+  ComputerDesktopIcon,
+  DocumentTextIcon,
+  FolderIcon,
+  FolderOpenIcon,
+  HomeIcon,
+} from "@heroicons/react/16/solid";
+import { Ubuntu } from "next/font/google";
+import Link from "next/link";
+
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["300", "500"] });
+
 const Sidebar = () => {
   return (
     <div className="bg-zinc-50 dark:bg-zinc-900 p-4 fixed top-14 w-64 left-0 h-screen border-r">
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae similique
-        nobis accusantium dolor! Tenetur praesentium sunt quasi voluptatibus id
-        voluptas!
-      </h1>
+      <ul
+        className={`flex gap-4 items-center justify-center flex-col pt-20 ${ubuntu.className}`}
+      >
+        <Link
+          href={"/"}
+          className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
+        >
+          <HomeIcon className="w-6 h-6" />
+          Bosh Sahifa
+        </Link>
+        <Link
+          href={"/articles"}
+          className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
+        >
+          {/* <CodeBracketSquareIcon className="w-6 h-6"/> */}
+          <DocumentTextIcon className="w-6 h-6"/>
+          Maqolalar
+        </Link>
+        <Link
+          href={"/source_code"}
+          className="flex text-lg items-center gap-1 border p-3 w-full mx-auto border-[#3bc43f] rounded-md hover:border-[#288829]"
+        >
+          <CodeBracketSquareIcon className="w-6 h-6"/>
+          Kod Manbalari
+        </Link>
+        <Link
+          href={"/courses"}
+          className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
+        >
+          <ComputerDesktopIcon className="w-6 h-6"/>
+          Barch kurslar
+        </Link>
+        <Link
+          href={"/projects"}
+          className="flex text-lg items-center gap-1 p-3 w-full mx-auto rounded-md transition-all duration-[2000ms] ease-in-out dark:hover:bg-zinc-800"
+        >
+          <FolderIcon className="w-6 h-6"/>
+          Loyihalar
+        </Link>
+      </ul>
+      <Link
+        href="/login"
+        className={`${ubuntu.className} absolute bottom-20 flex text-lg items-center gap-1 p-2 rounded-md transition-all border duration-500 ease-in-out dark:hover:bg-[#3bc43f]`}
+      >
+        Dasturga kirish <ArrowRightEndOnRectangleIcon className="w-6 h-6" />
+      </Link>
     </div>
   );
 };
