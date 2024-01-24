c0aa9fda16769a66e8a1503788cd8ce9d91a6777
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-24 17:36:25 +0500

    added template of course

---
+++ src/app/course/[name]/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const CourseDetails = () => {
+  return (
+    <div className='lg:ml-64 md:ml-20 sm:ml-24'>
+        <h2>CourseDetails</h2>
+        <Link href={'/course/react/dashboard/dcdddcdcdsxscds'}>KIRISH</Link>
+    </div>
+  )
+}
+
+export default CourseDetails
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,11 +1,13 @@
-"use client";
-import ReactPlayer from "react-player";
+import Image from "next/image";
+import Link from "next/link";
+
+// import ReactPlayer from "react-player";
 
 const Courses = () => {
   let videosrc = "/videos/lesson2.mp4";
   return (
     <div className="lg:ml-64 md:ml-20 sm:ml-24">
-      <ReactPlayer
+      {/* <ReactPlayer
         width="500px"
         height="400px"
         url={videosrc}
@@ -15,7 +17,106 @@ const Courses = () => {
         // picture in picture
         pip={true}
       />
-      <source src={videosrc} type="video/mp4" />
+      <source src={videosrc} type="video/mp4" /> */}
+      <h2>Kurslar</h2>
+      <div className="flex items-center gap-3 flex-wrap">
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+        <Link
+          href="course/react"
+          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
+        >
+          <Image
+            src="/courses/react.png"
+            alt="react"
+            width={150}
+            height={150}
+          />
+          <h3 className="font-bold text-md">ReactJS</h3>
+        </Link>
+      </div>
     </div>
   );
 };
