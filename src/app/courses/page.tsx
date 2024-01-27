19ca3e58f777e8079b0d7695a15d46a46d522c96
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-27 11:46:17 +0500

    added hovercard

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,31 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 // import ReactPlayer from "react-player";
-interface Video {
-  id: string;
-  title: string;
-  path: string;
-}
-
-interface VideoCategory {
-  [key: string]: Video[];
-}
 
 const Courses = async () => {
-  // let videosrc = "/videos/lesson2.mp4";
-  try {
-    const res = await fetch("/public/data/index.json");
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data: ${res.statusText}`);
-    }
-
-    const videos: VideoCategory = await res.json();
-    console.log(videos);
-  } catch (error) {
-    console.error(error);
-  }
-
   return (
     <div className="lg:ml-64 md:ml-20 sm:ml-24">
       {/* <ReactPlayer
@@ -41,18 +24,29 @@ const Courses = async () => {
       <source src={videosrc} type="video/mp4" /> */}
       <h2>Kurslar</h2>
       <div className="flex items-center gap-3 flex-wrap">
-        <Link
-          href="course/react"
-          className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
-        >
-          <Image
-            src="/courses/react.png"
-            alt="react"
-            width={150}
-            height={150}
-          />
-          <h3 className="font-bold text-md">ReactJS</h3>
-        </Link>
+        <HoverCard>
+          <HoverCardTrigger className="relative">
+            <Link
+              href="course/react"
+              className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center justify-center bg-zinc-50 dark:bg-zinc-900"
+            >
+              <Image
+                src="/courses/react.png"
+                alt="react"
+                width={150}
+                height={150}
+              />
+              <h3 className="font-bold text-md">ReactJS</h3>
+            </Link>
+          </HoverCardTrigger>
+          <HoverCardContent className="absolute bottom-2">
+            <div className="line-clamp-2">
+              Hozirda mashhur JavaScript frameworklaridan biri ReactJSni to'liq
+              o'rganing. ReactJS darslari to'liq kurs o'zbek tilida. ReactJS
+              amaliyot va loyihalar
+            </div>
+          </HoverCardContent>
+        </HoverCard>
         <Link
           href="course/react"
           className="flex flex-col gap-2 shadow-md lg:h-64 lg:w-64 p-6 rounded-md border items-center   justify-center"
